7788a62b0b595ae36ff5768955efc121cd3141b7
Simon Howard <fraggle@soulsphere.org>
2023-04-21 19:10:44 -0400

    unifdef: -DS_ISUID
    POSIX-defined macro.
    
    Full command:
    unifdef -B -m *.[ch] unix/*.[ch] \
    -U__16BIT__ -U__386BSD__ -UACORN_GUI -Uaiws -U_AIX -UAMIGA -UAOS_VS \
    -Uapollo -U__APPLE__ -UASM_INFLATECODES -UATARI -UATARI_ST -UATH_BEO \
    -DATH_BEO_THS_UNX -DATH_BEO_UNX -DATH_BE_UNX -U__ATHEOS__ -U__BEOS__ \
    -U__BORLANDC__ -UBORLAND_STAT_BUG -UBSD -Ubsd4_2 -UBSD4_4 -U__bsdi__ \
    -DCdecl -UCHECK_VERSIONS -UNO_CHMOD -UCMS_MVS -UCOHERENT \
    -U__COMPILER_KCC__ -DUSE_CONST -UNO_CONST -UCONVEX -U__convex__ \
    -U__convexc__ -UCRAY -Ucray -U__CYGWIN__ -U__DECC_VER -UDECLARE_ERRNO \
    -U__DGUX__ -UNO_DIR -DDIRENT -U__DJGPP__ -UDLL -UDNIX \
    -UDOS_FLX_H68_NLM_OS2_W32 -UDOS_FLX_H68_OS2_W32 -UDOS_FLX_NLM_OS2 \
    -UDOS_FLX_NLM_OS2_W32 -UDOS_FLX_OS2 -UDOS_FLX_OS2_W32 \
    -UDOS_H68_OS2_W32 -UDOS_NLM_OS2_W32 -UDOS_OS2 -UDOS_OS2_W32 \
    -UDOS_T20_VMS -UEFT -DUSE_EF_UT_TIME -U__EMX__ -UNO_EXCEPT_SIGNALS \
    -UNO_FCHMOD -UNO_FCHOWN -UNO_FCNTL_H -UFLEXOS -U__FreeBSD__ \
    -UUSE_FWRITE -UNO_GMTIME -U__GO32__ -DGOT_UTIMBUF -Ugould -U__HP_cc \
    -U__hpux -U__human68k__ -U__i386 -U__i386__ -Ui386 -U__i486 -U__i486__ \
    -Ui486 -U__i586 -U__i586__ -Ui586 -U__i686 -U__i686__ -Ui686 \
    -U__IBMC__ -UINT_16BIT -DINT_SPRINTF -UIZ_CHECK_TZ \
    -UIZTZ_DEFINESTDGLOBALS -UIZTZ_GETLOCALETZINFO -UIZTZ_SETLOCALTZINFO \
    -UNO_LANGINFO_H -DNO_LCHMOD -UNO_LCHOWN -UNO_LIMITS_H -ULINUX -Ulinux \
    -ULynx -Umacintosh -UMACOS -Umain -UMED_MEM -UM_I86 -UMINIX -U__MINT__ \
    -UNO_MKDIR -DHAVE_MKTIME -DMODERN -U__mpexl -UMPW -UMSC -U_MSC_VER \
    -UMSDOS -U__MSDOS__ -UM_SYS5 -UM_SYSV -UMTS -UM_UNIX -UMVS \
    -U__MWERKS__ -UM_XENIX -UNEED__ISINDST -U__NetBSD__ -UNeXT \
    -UNGROUPS_MAX -UNO_NL_LANGINFO -UNLM -UNTSD_EAS \
    -UNX_CURRENT_COMPILER_RELEASE -UNO_OFF_T -UOLD_THEOS_EXTRA -UOS2 \
    -U__OS2__ -UOS2DLL -UOS2_EAS -UOS2_W32 -UOS390 -U__osf__ \
    -UPCHAR_SPRINTF -UPOCKET_UNZIP -DUSE_POSIX_TERMIOS -D_POSIX_VERSION \
    -U__POWERC -DPROTO -UNOPROTO -UPTX -Upyr -Upyr_bsd -UQDOS -UQLZIP \
    -U__QNX__ -U__QNXNTO__ -UQUERY_TRNEWLN -Urealix -UREALLY_SHORT_SYMS \
    -UREGULUS -DRESTORE_UIDGID -URETURN_CODES -URISCOS -U__riscos \
    -U__RSXNT__ -USCO_XENIX -DSEEK_SET -U_SEQUENT -U_SEQUENT_ \
    -DSET_DIR_ATTRIB -UNO_SETLOCALE -DSET_SYMLINK_ATTRIBS -U__sgi -Usgi \
    -USHORT_NAMES -USHORT_SYMS -DS_IFLNK -DS_ISDIR -DS_ISGID -DS_ISLNK \
    -DS_ISUID

--- unix/unix.c
+++ unix/unix.c
@@ -215,9 +215,6 @@ char *do_wild(__G__ wildspec)
 
 
 
-#ifndef S_ISUID
-# define S_ISUID        0004000 /* set user id on execution */
-#endif
 #ifndef S_ISVTX
 # define S_ISVTX        0001000 /* save swapped text even after use */
 #endif
