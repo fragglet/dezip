91e4ae9724e4452d8a777b61c6c3e19de3ce362b
Simon Howard <fraggle@soulsphere.org>
2023-04-21 19:10:26 -0400

    unifdef: -DHAVE_MKTIME
    mktime() is a POSIX function and ISO C.
    
    Full command:
    unifdef -B -m *.[ch] unix/*.[ch] \
    -U__16BIT__ -U__386BSD__ -UACORN_GUI -Uaiws -U_AIX -UAMIGA -UAOS_VS \
    -Uapollo -U__APPLE__ -UASM_INFLATECODES -UATARI -UATARI_ST -UATH_BEO \
    -DATH_BEO_THS_UNX -DATH_BEO_UNX -DATH_BE_UNX -U__ATHEOS__ -U__BEOS__ \
    -U__BORLANDC__ -UBORLAND_STAT_BUG -UBSD -Ubsd4_2 -UBSD4_4 -U__bsdi__ \
    -DCdecl -UCHECK_VERSIONS -UNO_CHMOD -UCMS_MVS -UCOHERENT \
    -U__COMPILER_KCC__ -DUSE_CONST -UNO_CONST -UCONVEX -U__convex__ \
    -U__convexc__ -UCRAY -Ucray -U__CYGWIN__ -U__DECC_VER -UDECLARE_ERRNO \
    -U__DGUX__ -UNO_DIR -DDIRENT -U__DJGPP__ -UDLL -UDNIX \
    -UDOS_FLX_H68_NLM_OS2_W32 -UDOS_FLX_H68_OS2_W32 -UDOS_FLX_NLM_OS2 \
    -UDOS_FLX_NLM_OS2_W32 -UDOS_FLX_OS2 -UDOS_FLX_OS2_W32 \
    -UDOS_H68_OS2_W32 -UDOS_NLM_OS2_W32 -UDOS_OS2 -UDOS_OS2_W32 \
    -UDOS_T20_VMS -UEFT -DUSE_EF_UT_TIME -U__EMX__ -UNO_EXCEPT_SIGNALS \
    -UNO_FCHMOD -UNO_FCHOWN -UNO_FCNTL_H -UFLEXOS -U__FreeBSD__ \
    -UUSE_FWRITE -UNO_GMTIME -U__GO32__ -DGOT_UTIMBUF -Ugould -U__HP_cc \
    -U__hpux -U__human68k__ -U__i386 -U__i386__ -Ui386 -U__i486 -U__i486__ \
    -Ui486 -U__i586 -U__i586__ -Ui586 -U__i686 -U__i686__ -Ui686 \
    -U__IBMC__ -UINT_16BIT -DINT_SPRINTF -UIZ_CHECK_TZ \
    -UIZTZ_DEFINESTDGLOBALS -UIZTZ_GETLOCALETZINFO -UIZTZ_SETLOCALTZINFO \
    -UNO_LANGINFO_H -DNO_LCHMOD -UNO_LCHOWN -UNO_LIMITS_H -ULINUX -Ulinux \
    -ULynx -Umacintosh -UMACOS -Umain -UMED_MEM -UM_I86 -UMINIX -U__MINT__ \
    -UNO_MKDIR -DHAVE_MKTIME

--- fileio.c
+++ fileio.c
@@ -1549,8 +1549,6 @@ time_t dos_to_unix_time(dosdatetime)
 {
     time_t m_time;
 
-#ifdef HAVE_MKTIME
-
     ZCONST time_t now = time(NULL);
     struct tm *tm;
 #   define YRBASE  1900
@@ -1571,127 +1569,6 @@ time_t dos_to_unix_time(dosdatetime)
     m_time = mktime(tm);
     NATIVE_TO_TIMET(m_time)     /* NOP unless MSC 7.0 or Macintosh */
     TTrace((stderr, "  final m_time  =       %lu\n", (ulg)m_time));
-
-#else /* !HAVE_MKTIME */
-
-    int yr, mo, dy, hh, mm, ss;
-#ifdef TOPS20
-#   define YRBASE  1900
-    struct tmx *tmx;
-    char temp[20];
-#else /* !TOPS20 */
-#   define YRBASE  1970
-    int leap;
-    unsigned days;
-    struct tm *tm;
-#if (!defined(MACOS) && !defined(RISCOS) && !defined(QDOS) && !defined(TANDEM))
-#ifdef WIN32
-    TIME_ZONE_INFORMATION tzinfo;
-    DWORD res;
-#else /* ! WIN32 */
-#if (defined(BSD) || defined(MTS) || defined(__GO32__))
-    struct timeb tbp;
-#else /* !(BSD || MTS || __GO32__) */
-#ifdef DECLARE_TIMEZONE
-    extern time_t timezone;
-#endif
-#endif /* ?(BSD || MTS || __GO32__) */
-#endif /* ?WIN32 */
-#endif /* !MACOS && !RISCOS && !QDOS && !TANDEM */
-#endif /* ?TOPS20 */
-
-
-    /* dissect date */
-    yr = ((int)(dosdatetime >> 25) & 0x7f) + (1980 - YRBASE);
-    mo = ((int)(dosdatetime >> 21) & 0x0f) - 1;
-    dy = ((int)(dosdatetime >> 16) & 0x1f) - 1;
-
-    /* dissect time */
-    hh = (int)((unsigned)dosdatetime >> 11) & 0x1f;
-    mm = (int)((unsigned)dosdatetime >> 5) & 0x3f;
-    ss = (int)((unsigned)dosdatetime & 0x1f) * 2;
-
-#ifdef TOPS20
-    tmx = (struct tmx *)malloc(sizeof(struct tmx));
-    sprintf (temp, "%02d/%02d/%02d %02d:%02d:%02d", mo+1, dy+1, yr, hh, mm, ss);
-    time_parse(temp, tmx, (char *)0);
-    m_time = time_make(tmx);
-    free(tmx);
-
-#else /* !TOPS20 */
-
-/*---------------------------------------------------------------------------
-    Calculate the number of seconds since the epoch, usually 1 January 1970.
-  ---------------------------------------------------------------------------*/
-
-    /* leap = # of leap yrs from YRBASE up to but not including current year */
-    leap = ((yr + YRBASE - 1) / 4);   /* leap year base factor */
-
-    /* calculate days from BASE to this year and add expired days this year */
-    days = (yr * 365) + (leap - 492) + ydays[mo];
-
-    /* if year is a leap year and month is after February, add another day */
-    if ((mo > 1) && ((yr+YRBASE)%4 == 0) && ((yr+YRBASE) != 2100))
-        ++days;                 /* OK through 2199 */
-
-    /* convert date & time to seconds relative to 00:00:00, 01/01/YRBASE */
-    m_time = (time_t)((unsigned long)(days + dy) * 86400L +
-                      (unsigned long)hh * 3600L +
-                      (unsigned long)(mm * 60 + ss));
-      /* - 1;   MS-DOS times always rounded up to nearest even second */
-    TTrace((stderr, "dos_to_unix_time:\n"));
-    TTrace((stderr, "  m_time before timezone = %lu\n", (ulg)m_time));
-
-/*---------------------------------------------------------------------------
-    Adjust for local standard timezone offset.
-  ---------------------------------------------------------------------------*/
-
-#if (!defined(MACOS) && !defined(RISCOS) && !defined(QDOS) && !defined(TANDEM))
-#ifdef WIN32
-    /* account for timezone differences */
-    res = GetTimeZoneInformation(&tzinfo);
-    if (res != TIME_ZONE_ID_INVALID)
-    {
-    m_time += 60*(tzinfo.Bias);
-#else /* !WIN32 */
-#if (defined(BSD) || defined(MTS) || defined(__GO32__))
-    ftime(&tbp);                                /* get `timezone' */
-    m_time += tbp.timezone * 60L;               /* seconds WEST of GMT:  add */
-#else /* !(BSD || MTS || __GO32__) */
-    /* tzset was already called at start of process_zipfiles() */
-    /* tzset(); */              /* set `timezone' variable */
-    m_time += timezone;         /* seconds WEST of GMT:  add */
-#endif /* ?(BSD || MTS || __GO32__) */
-#endif /* ?WIN32 */
-    TTrace((stderr, "  m_time after timezone =  %lu\n", (ulg)m_time));
-
-/*---------------------------------------------------------------------------
-    Adjust for local daylight savings (summer) time.
-  ---------------------------------------------------------------------------*/
-
-    if ( (dosdatetime >= DOSTIME_2038_01_18) &&
-         (m_time < (time_t)0x70000000L) )
-        m_time = U_TIME_T_MAX;  /* saturate in case of (unsigned) overflow */
-    if (m_time < (time_t)0L)    /* a converted DOS time cannot be negative */
-        m_time = S_TIME_T_MAX;  /*  -> saturate at max signed time_t value */
-    TIMET_TO_NATIVE(m_time)     /* NOP unless MSC 7.0 or Macintosh */
-    if (((tm = localtime((time_t *)&m_time)) != NULL) && tm->tm_isdst)
-#ifdef WIN32
-        m_time += 60L * tzinfo.DaylightBias;    /* adjust with DST bias */
-    else
-        m_time += 60L * tzinfo.StandardBias;    /* add StdBias (normally 0) */
-#else
-        m_time -= 60L * 60L;    /* adjust for daylight savings time */
-#endif
-    NATIVE_TO_TIMET(m_time)     /* NOP unless MSC 7.0 or Macintosh */
-    TTrace((stderr, "  m_time after DST =       %lu\n", (ulg)m_time));
-#ifdef WIN32
-    }
-#endif
-#endif /* !MACOS && !RISCOS && !QDOS && !TANDEM */
-#endif /* ?TOPS20 */
-
-#endif /* ?HAVE_MKTIME */
 
     if ( (dosdatetime >= DOSTIME_2038_01_18) &&
          (m_time < (time_t)0x70000000L) )
