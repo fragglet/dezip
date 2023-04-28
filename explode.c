685afe208061c523db797219a53885ed792be496
Simon Howard <fraggle@soulsphere.org>
2023-04-27 23:59:30 -0400

    Delete "copyright history".
    It's under the Info-ZIP license now, and that's what matters.

--- explode.c
+++ explode.c
@@ -7,17 +7,7 @@
 /* explode.c -- by Mark Adler
    version c17d, 01 December 2007 */
 
-/* Copyright history:
-   - Starting with UnZip 5.41 of 16-April-2000, this source file
-     is covered by the Info-Zip LICENSE cited above.
-   - Prior versions of this source file, found in UnZip source packages
-     up to UnZip 5.40, were put in the public domain.
-     The original copyright note by Mark Adler was:
-         "You can do whatever you like with this source file,
-         though I would prefer that if you modify it and
-         redistribute it that you include comments to that effect
-         with your name and the date.  Thank you."
-
+/*
    History:
    vers    date          who           what
    ----  ---------  --------------  ------------------------------------
